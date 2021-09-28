03a0e5369f756c069be3aaab0393c37b7f9442fb
Atishay jain <atishayjain78980@gmail.com>
2021-09-29 00:37:31 +0530

    1st Testcase passed

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,25 @@
 
 public class Calculator {
-
-    public int calculate(String string){
+    public int Add(String input){   
+    	String[] numbers = input.split(",");   
+        if(isEmpty(input)){
             return 0;
+        }
+        if(input.length()==1){
+            return stringToint(input);
+        }
+        
+        else{
+            return Integer.parseInt(numbers[0])+ Integer.parseInt(numbers[1]);
+        }
     } 
+        
+    private int stringToint(String input){
+        return Integer.parseInt(input);
+    }
+    
+    private boolean isEmpty(String input){
+        return input.isEmpty();
+    }
+	
 }
--- src/TestCalculator.java
+++ src/TestCalculator.java
@@ -1,13 +1,26 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 
 @Test
 public class TestCalculator {
+    private Calculator calculator;
+    
+    @BeforeTest
+    public void init(){
+        calculator = new Calculator();
+    }
 	public void EmptyStringReturnZero() {
-		Calculator calculator = new Calculator();
-		assertEquals(calculator.calculate(""),0);
+		assertEquals(calculator.Add(""), 0);
 	}
 	
+	public void SingleValueIsReplied(){
+	    assertEquals(calculator.Add("1"), 1);
+	}
 	
+	public void TwoNumbersCommaDelReturnSum()
+	{
+	    assertEquals(calculator.Add("1,2"), 3);
+	}	
 }
